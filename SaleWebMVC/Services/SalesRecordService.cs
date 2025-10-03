402364aac9ce75cb08e81e5f6df765013a4b3f2a
Yan Bandeira <yanbuenobandeira@gmail.com>
2025-10-03 16:30:34 -0300

    Preparando o campo de procura das vendas na aba de navegação

---
+++ SaleWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,20 @@
+﻿using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SalesRecordService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Sales> FindByDate(DateTime date)
+        {
+
+        }
+    }
+}
